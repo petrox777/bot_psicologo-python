0775816cb4e3be390f37318e43a7d126d1767a64
petrox777 <pedro.macedo.ismart@gmail.com>
2026-05-30 20:57:30 -0300

    atualizei

--- bot.py
+++ bot.py
@@ -1,9 +1,50 @@
 import time
 from respostas import ( # Importando as listas de respostas 
-    saudacoes, tristeza, felicidade, neutras, desconhecido, despedida, raiva, nãoidentificado, escolher
+    saudacoes, tristeza, felicidade, neutras, despedida, raiva, não_identificado, de_tristeza_para_raiva, de_tristeza_para_felicidade, triste_persistente, de_raiva_para_tristeza, de_raiva_para_felicidade, de_felicidade_para_raiva, de_felicidade_para_tristeza, de_tristeza_para_neutralidade, escolher
 )
+def interpretar_memoria(memoria):
+
+    if {"contador_triste"}>= 5:
+        return "triste_pesado"
+
+    elif {"contador_feliz"} > 3:
+        return "felicidade"
+    
+    elif {"contador_triste"} >= 3:
+        return "triste_persistente"
+    
+    elif {"contador_feliz"} >= 3:
+        return "muita_felicidade"
+    
+    elif {"contador_raiva"} >= 3:
+        return "raiva_persistente"
+    
+    elif {"humor_anterior"} == "triste" and {"humor_atual"} == "raiva":
+        return "de_tristeza_para_raiva"
+    
+    elif {"humor_anterior"} == "triste" and {"humor_atual"} == "feliz":
+        return "de_tristeza_para_felicidade"
+    
+    elif {"humor_anterior"} == "raiva" and {"humor_atual"} == "triste":
+        return "de_raiva_para_tristeza"
+    
+    elif {"humor_anterior"} == "raiva" and {"humor_atual"} == "feliz":
+        return "de_raiva_para_felicidade"
+    
+    elif {"humor_anterior"} == "feliz" and {"humor_atual"} == "raiva":
+        return "de_felicidade_para_raiva"
+    
+    elif {"humor_anterior"} == "feliz" and {"humor_atual"} == "triste":
+        return "de_felicidade_para_tristeza"            
+    
+    elif {"humor_anterior"} == "triste" and {"humor_atual"} == "neutro":
+        return "de_tristeza_para_neutralidade"
+    
+    return "não_identificado"
+
 
 def iniciar_bot(): 
+
     nome = None # Variável para armazenar o nome do usuário
 
     # Inicializando a variável memoria
@@ -34,41 +75,60 @@ def iniciar_bot():
         user = input("> ").lower() # Lendo a entrada do usuário
 
         if any(p in user for p in ["sair", "exit", "quit"]):
-            resposta = escolher(despedida, nome)
-            print(resposta)
+            print(escolher(despedida, nome))
             break
 
         if any(p in user for p in ["oi", "ola", "salve", "opa", "hey"]):
-            resposta = print(escolher(saudacoes, nome))
+            print(escolher(saudacoes, nome))
 
-        elif any(p in user for p in ["triste", "mal", "chateado", "sozinho"]):
-            memoria["humor"] = "triste"
+        if any(p in user for p in ["triste", "mal", "chateado", "sozinho"]):
+            memoria["humor_anterior"] = memoria["humor_atual"] 
+            memoria["humor_atual"] = "triste"
             memoria["contador_triste"] += 1
+
+            
+            if estado == "de_tristeza_para_raiva":
+                print(escolher(de_tristeza_para_raiva, nome))
+
+            elif estado == "de_tristeza_para_felicidade":
+                print(escolher(de_tristeza_para_felicidade, nome))
+
+            elif estado == "triste_persistente":
+                 print(escolher(triste_persistente, nome))    
+
 
             print("DEBUG -> contador_triste:", memoria["contador_triste"])
 
             if memoria["contador_triste"] >= 5:
                 print(f"{nome}, fica assim não, vc é d++, bola pra frente")
             elif memoria["contador_triste"] >= 3 and memoria["contador_feliz"] >= 0:
-                print("triste_persistente")
+                print( f"{nome}, se a vida fosse uma constante de prazer e gozo não valeria a pena, os desafios são feitos para nos forjar, assim como disse Nietzsche- Aquilo que não me mata só me deixa mais forte")
             else:
                 print(escolher(tristeza, nome))
     
 
         elif any(p in user for p in ["feliz", "top", "bom", "ótimo"]):
-            memoria["humor"] = "feliz"
+            memoria["humor_anterior"] = memoria["humor_atual"]
+            memoria["humor_atual"]= "feliz"
             memoria["contador_feliz"] += 1
 
-            if memoria["contador_feliz"] >= 2:
-             print(f"oi, {nome}, ai sim pô, ninguém te para!!!")
-            else:
-               resposta = print(escolher(felicidade, nome))
+            if estado =="de_tristeza_para_felicidade":
+               print(escolher(de_tristeza_para_felicidade, nome))
+
+            if estado == "de_raiva_para_felicidade":
+               print(escolher(de_raiva_para_felicidade, nome))         
+
+            if memoria["contador_feliz"] >= 3:
+               print(f"oi, {nome}, ai sim pô, ninguém te para!!!")
+
+            if estado == "felicidade":
+               print(escolher(felicidade, nome))
 
         elif any(p in user for p in ["nada", "de boa", "suave"]):
-            resposta = print(escolher(neutras, nome))
+            print(escolher(neutras, nome))
 
         elif any(p in user for p in ["raiva", "furia", "revoltado" , "revoltada"]):
-            resposta = print(escolher(raiva, nome))
+            print(escolher(raiva, nome))
             memoria["contador_raiva"] += 1        
            
             if memoria["contador_raiva"] >= 3:
@@ -76,8 +136,7 @@ def iniciar_bot():
 
 
         elif user.strip() == "":
-            resposta = print(escolher(nãoidentificado, nome))
+           print(escolher(não_identificado, nome))
         
         else:
-            resposta = print(escolher(nãoidentificado, nome))
-
+           print(escolher(não_identificado, nome))
--- respostas.py
+++ respostas.py
@@ -24,12 +24,6 @@ neutras = [
     "Tô ouvindo, {nome}."
 ]
 
-desconhecido = [
-    "Não sei se entendi direito, {nome}, mas continua.",
-    "Fala mais sobre isso, {nome}.",
-    "Às vezes é confuso mesmo, {nome}."
-]
-
 raiva = [
          "calma ai, {nome}, vai dar tudo certo.",
          "fique de boa {nome}, no final o bem sempre vence.",
@@ -42,40 +36,52 @@ despedida = [
     "Foi bom falar contigo, {nome}. Até a próxima!" 
 ]
 
-nãoidentificado = [ "não entendi {nome}, qual o seu sentimento de agora?"
-                   "como assim {nome}? Como você está se sentindo"
+não_identificado = [ "não entendi {nome}, qual o seu sentimento de agora?",
+                   "como assim {nome}? Como você está se sentindo",
+                   "Não sei se entendi direito, {nome}, mas continua.",
+                   "Fala mais sobre isso, {nome}.",
+                   "Às vezes é confuso mesmo, {nome}."
 ]
 
-defelicidadeparatristeza = [
+de_felicidade_para_tristeza = [
     "{nome}, a vida da dessas, assim como uma montanha russa, o mais importaante é entender seus sentimentos e tentar mudar a situação"
     "{nome}, assim como sua felicidade sumiu ela aparecerá novamente, #tudopassa"
 
 ]                   
 
-detristezaparafelicidade = [
+de_tristeza_para_felicidade = [
     "Ai sim {nome} fico feliz que você está bem agora, assim que se faz"    
 
 ] 
  
-deraivaparatristeza = [
+de_raiva_para_tristeza = [
     "Os seus animos estão acalmando, você vai encontrar a melhor forma de contornar essa situação #confio"
 
 ] 
 
-deraivaparafelicidade = [
+de_raiva_para_felicidade = [
     "Ai sim {nome}, deixou aquela revolta de lado, o melhor é viver feliz e bem consigo mesmo"
 ]
 
-defelicidadepararaiva = [
+de_felicidade_para_raiva = [
     "{nome}, no mesmo nível de intensidade de felicidade que você estava agora, use essa revolta a seu favor e resolva esse problema (sem perder a cabeça, é claro #vcconsegue)"
 ]
 
-detristezapararaiva = [
+de_tristeza_para_raiva = [
     "{nome}, sei que não está fácil, mas tente relaxar antes que você faça algo que se arrependa futuramente"
+]
+
+de_tristeza_para_neutralidade = [
+    "{nome},isso é normal, às vezes a gente se sente pra baixo, mas isso não define quem você é. Tente focar nas coisas boas que tem na sua vida, mesmo que sejam pequenas."
 ]
 
 triste_persistente = [
     "{nome}, se a vida fosse uma constante de prazer e gozo não valeria a pena, os desafios são feitos para nos forjar, assim como disse Nietzsche- Aquilo que não me mata só me deixa mais forte"
+]
+
+muita_felicidade = [
+    "{nome}, vamos!!! É assim que se faz, continue com esse pensamento"
+
 ]
 
 def escolher(lista, nome):
